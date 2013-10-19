8abbc0b01360efb24b68b4153f7a5d2532119979
Gabriel JUCHAULT <gabriel_juchault@msn.com>
2013-10-20 00:36:52 +0200

    Update fuzzy.js

--- fuzzy.js
+++ fuzzy.js
@@ -14,43 +14,45 @@ var arr = ['A sentence composed of multiple words', 'Set syntax Javscript'];
 */
 
 // Improved from Bulat Bochkariov's version (http://www.quora.com/Algorithms/How-is-the-fuzzy-search-algorithm-in-Sublime-Text-designed)
-function fuzzy (searchSet, query, doNotHighlight, highlighter) {
-    if (!query) return searchSet;
-    if (!highlighter) {
-        highlighter = function (str) {
-            return '<span>' + str + '</span>';
+;(function (window) {
+    window.fuzzy = function (searchSet, query, doNotHighlight, highlighter) {
+        if (!query) return searchSet;
+        if (!highlighter) {
+            highlighter = function (str) {
+                return '<span>' + str + '</span>';
+            }
         }
-    }
-
-    var tokens = query.toLowerCase().split(''),
-        matches = [];
-
-    // Better use forEach here, length may be invalid
-    searchSet.forEach(function (string) {
-        var tokenIndex = 0,
-            stringIndex = 0,
-            matchWithHighlights = '',
-            matchedPositions = [],
-            stringLC = string.toLowerCase();
-
-        while (stringIndex < string.length) {
-            if (stringLC[stringIndex] === tokens[tokenIndex]) {
-                matchWithHighlights += (doNotHighlight) ? string[stringIndex] : highlighter(string[stringIndex]);
-                matchedPositions.push(stringIndex);
-                tokenIndex++;
-
-                if (tokenIndex >= tokens.length) {
-                    matches.push(matchWithHighlights + string.slice(stringIndex + 1));
-                    break;
+    
+        var tokens = query.toLowerCase().split(''),
+            matches = [];
+    
+        // Better use forEach here, length may be invalid
+        searchSet.forEach(function (string) {
+            var tokenIndex = 0,
+                stringIndex = 0,
+                matchWithHighlights = '',
+                matchedPositions = [],
+                stringLC = string.toLowerCase();
+    
+            while (stringIndex < string.length) {
+                if (stringLC[stringIndex] === tokens[tokenIndex]) {
+                    matchWithHighlights += (doNotHighlight) ? string[stringIndex] : highlighter(string[stringIndex]);
+                    matchedPositions.push(stringIndex);
+                    tokenIndex++;
+    
+                    if (tokenIndex >= tokens.length) {
+                        matches.push(matchWithHighlights + string.slice(stringIndex + 1));
+                        break;
+                    }
                 }
+                else {
+                    matchWithHighlights += string[stringIndex];
+                }
+    
+                stringIndex++;
             }
-            else {
-                matchWithHighlights += string[stringIndex];
-            }
-
-            stringIndex++;
-        }
-    });
-
-    return matches;
-}
+        });
+    
+        return matches;
+    }
+}(window));
