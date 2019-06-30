d559c39d50c5179acb23183f78fae0dd389b045d
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2019-06-30 14:47:13 +0200

    chore(docs): add comment blocks to enhance docs (#26)

--- src/surround.ts
+++ src/surround.ts
@@ -1,5 +1,11 @@
 import { MatchRange } from './match'
 
+/**
+ * This represents the options you can pass to surround.
+ * It requires at least `result` (that you'll get in `match` with the
+ * `withRanges` option set to true). You can give a prefix and a suffix, which
+ * will surround every part of the source string that matched the query.
+ */
 export interface SurroundOptions {
   result: {
     ranges: MatchRange[]
@@ -8,9 +14,20 @@ export interface SurroundOptions {
   suffix?: string
 }
 
+/**
+ * @ignore
+ */
 const insertAt = (input: string, index: number, patch: string = '') =>
   input.slice(0, index) + patch + input.slice(index)
 
+/**
+ * Surround parts of the string that matched with prefix and suffix.
+ * Useful to emphasize the parts that matched.
+ *
+ * @param source The input source
+ * @param options Options as defined by [[SurroundOptions]]
+ * @returns The input source with matching ranges surrounded by prefix and suffix
+ */
 export const surround = (source: string, options: SurroundOptions) => {
   if (typeof source !== 'string') {
     throw new TypeError('Expecting source to be a string')
